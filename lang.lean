4013a94739478661db346fdbba11b84a2621c3c9
DKormann <dominikormann@outlook.com>
2025-07-22 01:18:06 +0200

    wip

--- lang.lean
+++ lang.lean
@@ -1,55 +1,42 @@
-inductive Vec (A:Type): Nat -> Type
-| nil : Vec A 0
-| cons: A -> Vec A n -> Vec A (n+1)
 
 
-def Vec.append {A : Type} {m n : Nat} : Vec A m → Vec A n → Vec A (m + n)
-| .nil, ys => by
-  rw [Nat.zero_add]
-  exact ys
-| .cons x xs, ys => by
-  rw [Nat.succ_add]
-  exact .cons x (append xs ys)
-
-instance {A : Type} {m n : Nat} : HAppend (Vec A m) (Vec A n) (Vec A (m + n)) where
-  hAppend := Vec.append
-
-def Vec.toList: Vec A n -> List A
-| nil => []
-| cons x xs => x :: Vec.toList xs
-
-def Vec.fromList {A}: List A -> (n:Nat) × Vec A n
-| [] => .mk 0 nil
-| x :: xs =>
-  let rest := Vec.fromList xs
-  .mk (rest.fst + 1) $ .cons x rest.snd
+def List.pop : (axis:Nat) -> (shp:List Nat) -> (List Nat)
+| _ , [] => []
+| 0, _::xs => xs
+| axis+1, x::xs => x :: List.pop axis xs
 
 
-def Vec.zeros (n) : Vec Nat n := match n with
-| 0 => .nil
-| n+1=> .cons 0 $ .zeros n
+inductive ZOp : Type
+| rand
+| full : (x:Nat) -> ZOp
 
+inductive UOp : Type
+| inv | exp
 
-instance[ToString A] : ToString (Vec A n)  where
-  toString := fun xs => "<Vec "++ toString (xs.toList) ++ " >"
+inductive BinOp : Type
+| add | sub | mul | div
 
-#eval (Vec.fromList [1,2,3]).snd
+inductive ReduceOp : Type
+| sum | prod | mean | max | min
 
-def Shape := Vec Nat
+inductive View : Type
+| permute : (axes: List Nat) -> View
 
+def View.apply (view:View) (shp:List Nat) : List Nat :=
+match view with
+| .permute axes => axes.map fun x => shp[x]!
 
-def Shape.pop : (i:Nat) -> (shp:Shape (n+1)) -> (Shape (n))
-| 0, .cons _ xs => xs
-| p+1, .cons x xs => match xs with
-  | .nil => .nil
-  | .cons y ys => .cons x (Shape.pop p (.cons y ys))
+inductive Tensor : (shp: List Nat) -> Type
+  | base : (op:ZOp) -> (shp: List Nat) -> Tensor shp
+  | unary : (op:UOp) -> (x:Tensor shp) -> Tensor shp
+  | binary : (op:BinOp) -> (x:Tensor shp) -> (y:Tensor shp) -> Tensor shp
+  | reduce : (op:ReduceOp) -> (axis:Nat) -> (x:Tensor shp) -> Tensor (shp.pop axis)
+  | reshape : (view : View) -> (x:Tensor shp) -> Tensor (view.apply shp)
 
+def Tensor.zeros (shp:List Nat) := Tensor.base (ZOp.full 0) shp
+def Tensor.ones (shp:List Nat) := Tensor.base (ZOp.full 1) shp
+def Tensor.sum (axis:Nat) (x:Tensor shp) := Tensor.reduce .sum axis x
 
-inductive Tensor : (shp:Shape n) -> Type
-| ones : (shp:Shape n) -> Tensor shp
-| sum : (axis:Nat) -> Tensor shp -> Tensor (shp.pop axis)
+def x := Tensor.ones [3,2]
 
-
-def x := Tensor.ones $ .cons 10 $ .cons 10 .nil
-
-def q : Tensor (.cons 10) := x.sum 0
+def y : Tensor [3] := Tensor.sum 0 x
